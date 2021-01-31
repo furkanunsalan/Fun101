989eb29d11a0bd765a2c1477c135ee92c1b81862
Furkan Ünsalan <76744098+Siflious@users.noreply.github.com>
2021-01-31 21:32:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -790,7 +790,51 @@ async def battle(ctx, member: discord.Member):
             if "teslim ol" in response.content.lower():
                   await ctx.send(f"{etiketlenen} **{etiketlenencan}** canla teslim oldu. {gonderen} **{gonderencan}** canı varken kazandı.")
                   break
-  
+"""
+Line 1-12 -> Imports -> Needed Imports For The Code
+Line 14-16 -> General Settings -> Game Activity, Prefix etc.
+Line 18-23 -> Reddit Praw Settings -> Needed Settings For 101meme
+Line 25-39 -> 101meme -> Takes A Random Post From r/dankmemes Subreddit
+Line 41-50 -> 101DM -> Sends The Written Text To Siflious#8620
+Line 51-58 -> 101geribildirim -> Shows How To Send Feedbacks
+Line 60-74 -> Bot On Ready -> Casual Things Bot Does When It Stars
+Line 76-93 -> 101şarkı -> Shows What The Tagged Person Is Listening
+Line 95-101 -> 101kaç -> Shows The Servers Bot Has Joined On Terminal
+Line 103-106 -> On Member Join -> Sends A Message When Someone Joins
+Line 108-111 -> On Member Remove -> Sends A Message When Someone Lefts
+Line 113-160 -> 101bilgi -> Shows Information About Tagged User
+Line 162-173 -> 101bot -> Shows Information About Fun101 Bot
+Line 175-194 -> 101tokatla Gifs -> Gif Links For 101tokatla
+Line 196-203 -> 101tokatla -> Slaps The Tagged Person
+Line 205-222 -> 101sarıl Gifs -> Gif Links For 101sarıl
+Line 224-231 -> 101sarıl -> Hugs The Tagged Person
+Line 233-254 -> 101zarvs -> Rolls 2 Dices And Sends A Winning Message
+Line 256-258 -> 101zar -> Rolls A Dice
+Line 260-264 -> 101tekrarla -> Repeats The Written Text
+Line 266-268 -> 101yazıtura -> Flips A Coin
+Line 270-358 -> 101tkm -> User Plays Rock, Paper, Scissors With Fun101  
+Line 360-469 -> 101tahmin -> Bot Chooses A Number And User Tries To Guess It
+Line 471-475 -> 101mood -> Decides Your Mood With Some Emojis
+Line 477-481 -> 101kahin -> Bot Replies To User Question With Random Answers
+Line 483-497 -> 101kick -> Kicks Tagged User If Author Has The Permission
+Line 499-513 -> 101ban -> Bans Tagged User If Author Has The Permission
+Line 515-524 -> 101muteall -> Mutes Every User In A Voice Channel
+Line 526-535 -> 101unmuteall -> Unmutes Every User In A Voice Channel
+Line 537-547 -> 101mute -> Mutes The Tagged User
+Line 549-559 -> 101unmute -> Unmutes The Tagged User
+Line 561-582 -> Gifs for 101gif -> Gif Links For 101gif
+Line 584-590 -> 101gif -> Sends A Random Gif
+Line 592-602 -> 101sunucu -> Gives Information About The Discord Server
+Line 604-610 -> 101sil -> Deletes Up To 50 Messages (User tells The Amount)
+Line 612-626 -> 101eğlence -> Shows All Fun Comments For Fun101
+Line 628-639 -> 101oyunlar -> Shows All Game Comments For Fun101
+Line 641-656 -> 101moderasyon -> Shows All Moderation Comments For Fun101
+Line 658-667 -> 101yardım -> Shows Help Comments For Fun101
+Line 669-792 -> 101dövüş -> Stars A Fight Between Tagged User And Message Author
+Line 793-837 -> Code Comments -> Explanations For All Of Fun101's Code
+Line 838-838 -> Keep Alive -> Keeps Fun101 Always Active
+Line 839-839 -> Bot Run -> But Token And The Function Needed To Run Fun101
+"""
 keep_alive()
 Bot.run("NzkwOTI5NTYzNTU5MDAyMTIy.X-Hwjg.cX_64XMhJfK8nO0RgeTwDRKo7qE")
     
